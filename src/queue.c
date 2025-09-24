759699540fad3694a20d44b67220e5bfabdfe781
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-09-24 15:26:25 +0200

    feat: add queue module

---
+++ src/queue.c
@@ -0,0 +1,60 @@
+#include <queue.h>
+#include <stdlib.h>
+
+typedef struct queue_item {
+	void			*data;
+	struct queue_item	*next;
+} QueueItem;
+
+Queue	*init_queue(void)
+{
+	Queue	*queue;
+
+	queue = malloc(sizeof(Queue));
+	if (!queue)
+		return (NULL);
+	queue->head = NULL;
+	queue->tail = NULL;
+	queue->size = 0;
+	return (queue);
+}
+
+int	enqueue(Queue *queue, void *data)
+{
+	QueueItem	*item;
+
+	item = malloc(sizeof(QueueItem));
+	if (!item)
+		return (0);
+	item->data = data;
+	item->next = NULL;
+	if (!queue->size)
+	{
+		queue->head = item;
+		queue->tail = item;
+	}
+	else
+	{
+		queue->tail->next = item;
+		queue->tail = item;
+	}
+	queue->size++;
+	return (1);
+}
+
+void	*dequeue(Queue *queue)
+{
+	QueueItem	*item;
+
+	if (!queue->size)
+		return (NULL);
+	item = queue->head;
+	queue->head = queue->head->next;
+	queue->size--;
+	return (item);
+}
+
+void	free_queue(Queue *queue)
+{
+	free(queue);
+}

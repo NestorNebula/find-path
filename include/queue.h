b12e25f62294aead0868a6d3c5a8d2d4f8535a15
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-09-24 15:14:15 +0200

    feat: add queue module's header

---
+++ include/queue.h
@@ -0,0 +1,42 @@
+#ifndef QUEUE_H
+# define QUEUE_H
+
+# include <stddef.h>
+
+typedef struct queue_item QueueItem;
+
+typedef struct queue {
+	QueueItem	*head;
+	QueueItem	*tail;
+	size_t		size;
+} Queue;
+
+/*
+ * init_queue:
+ * Initializes a queue and returns a pointer to it.
+ * Returns a NULL pointer if an error occurred.
+ */
+Queue	*init_queue(void);
+
+/*
+ * enqueue:
+ * Adds the data pointed by data to the end of the queue pointed by queue.
+ * Returns 1 if the data was added properly to the queue, else returns 0.
+ */
+int	enqueue(Queue *queue, void *data);
+
+/*
+ * dequeue:
+ * Removes the head of the queue pointed by queue and returns a pointer to
+ * the data stored inside of it.
+ * If the queue is empty, returns a NULL pointer instead.
+ */
+void	*dequeue(Queue *queue);
+
+/*
+ * free_queue:
+ * Frees the memory taken by the queue pointed by queue.
+ */
+void	free_queue(Queue *queue);
+
+#endif

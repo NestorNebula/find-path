16e9d0744c0d9a4524fae4088b59941fa54a2add
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-09-26 14:17:55 +0200

    feat: add input module's header

---
+++ include/input.h
@@ -0,0 +1,15 @@
+#ifndef INPUT_H
+# define INPUT_H
+
+/*
+ * read_input:
+ * Reads an input from fd until reaching 
+ * a closing parenthese or the EOF character.
+ * The input is expected to start with an open parenthese and
+ * to end with a closing parenthese. Any other input will be rejected.
+ * Returns the input as a string if no error occurred, 
+ * else returns a NULL pointer.
+ */
+char	*read_input(int fd);
+
+#endif

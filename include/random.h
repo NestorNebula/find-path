69d7f5403a569884e6af6c2cd67c478092d460d3
NestorNebula <160883075+NestorNebula@users.noreply.github.com>
2025-09-27 15:34:04 +0200

    feat: add random module's header

---
+++ include/random.h
@@ -0,0 +1,18 @@
+#ifndef RANDOM_H
+# define RANDOM_H
+
+/*
+ * init_random:
+ * Initializes the random list of numbers.
+ * This function must be called before calling random_number, 
+ * else random_number will always return the same numbers in the same order.
+ */
+void	init_random(void);
+
+/*
+ * random_number:
+ * Returns a random number modulo mod.
+ */
+int	random_number(int mod);
+
+#endif
